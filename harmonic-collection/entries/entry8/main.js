3987d550c0821b5de99588cd25396f3d1af4db6b
kims455 <kims455@newschool.edu>
2023-11-13 01:35:53 -0500

    Entry 8 Revision #2

--- harmonic-collection/entries/entry8/main.js
+++ harmonic-collection/entries/entry8/main.js
@@ -1,5 +1,11 @@
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 8; i++) {
     const rectangle = document.createElement('div');
     rectangle.classList.add('rectangle');
     document.body.appendChild(rectangle);
-  }
+}
+
+for (let i = 0; i < 15; i++) {
+    const pattern = document.createElement('div');
+    pattern.classList.add('pattern');
+    document.body.appendChild(pattern);
+}
