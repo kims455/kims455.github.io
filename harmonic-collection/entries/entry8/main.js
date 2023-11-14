a62b0f921153546d7c2c412fbd5f2b96e182add0
kims455 <kims455@newschool.edu>
2023-11-14 01:54:01 -0500

    Entry 8 Revision #5

--- harmonic-collection/entries/entry8/main.js
+++ harmonic-collection/entries/entry8/main.js
@@ -1,12 +1,30 @@
-for (let i = 0; i < 270; i++) {
+for (let i = 0; i < 81; i++) {
     const rectangle = document.createElement('div');
     rectangle.classList.add('square1');
     document.body.appendChild(rectangle);
 }
 
-for (let i = 0; i < 270; i++) {
+for (let i = 0; i < 81; i++) {
     const rectangle = document.createElement('div');
     rectangle.classList.add('square2');
+    document.body.appendChild(rectangle);
+}
+
+for (let i = 0; i < 81; i++) {
+    const rectangle = document.createElement('div');
+    rectangle.classList.add('square3');
+    document.body.appendChild(rectangle);
+}
+
+for (let i = 0; i < 81; i++) {
+    const rectangle = document.createElement('div');
+    rectangle.classList.add('square4');
+    document.body.appendChild(rectangle);
+}
+
+for (let i = 0; i < 81; i++) {
+    const rectangle = document.createElement('div');
+    rectangle.classList.add('square5');
     document.body.appendChild(rectangle);
 }
 
