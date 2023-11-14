5d8c7506e11bbf6da0e895142ab65fd8b84b6004
kims455 <kims455@newschool.edu>
2023-11-14 02:04:46 -0500

    Entry 8 Revision #6

--- harmonic-collection/entries/entry8/main.js
+++ harmonic-collection/entries/entry8/main.js
@@ -1,30 +1,35 @@
 for (let i = 0; i < 81; i++) {
     const rectangle = document.createElement('div');
     rectangle.classList.add('square1');
+    rectangle.style.animationDelay = Math.random() * 10 + 's'; 
     document.body.appendChild(rectangle);
 }
 
 for (let i = 0; i < 81; i++) {
     const rectangle = document.createElement('div');
     rectangle.classList.add('square2');
+    rectangle.style.animationDelay = Math.random() * 10 + 's'; 
     document.body.appendChild(rectangle);
 }
 
 for (let i = 0; i < 81; i++) {
     const rectangle = document.createElement('div');
     rectangle.classList.add('square3');
+    rectangle.style.animationDelay = Math.random() * 10 + 's'; 
     document.body.appendChild(rectangle);
 }
 
 for (let i = 0; i < 81; i++) {
     const rectangle = document.createElement('div');
     rectangle.classList.add('square4');
+    rectangle.style.animationDelay = Math.random() * 10 + 's'; 
     document.body.appendChild(rectangle);
 }
 
 for (let i = 0; i < 81; i++) {
     const rectangle = document.createElement('div');
     rectangle.classList.add('square5');
+    rectangle.style.animationDelay = Math.random() * 10 + 's'; 
     document.body.appendChild(rectangle);
 }
 
