e09c4f70cc98c4b80e93b602807965219925cc79
kims455 <kims455@newschool.edu>
2023-11-21 00:10:32 -0500

    Entry 9 Revision #9

--- harmonic-collection/entries/entry9/main.js
+++ harmonic-collection/entries/entry9/main.js
@@ -5,3 +5,9 @@ alert(window.innerWidth)
 alert(window.location.href)
 
 */
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      document.getElementById("overlay").classList.add("hidden");
+    }, 1800);
+  });
