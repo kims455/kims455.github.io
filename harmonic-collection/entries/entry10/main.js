105ee7b4b4b92b4843a89b20b38cac89cc81737c
kims455 <kims455@newschool.edu>
2023-11-27 23:17:53 -0500

    Entry 10 Revision #6

--- harmonic-collection/entries/entry10/main.js
+++ harmonic-collection/entries/entry10/main.js
@@ -17,12 +17,15 @@
     function calculateDays() {
       const targetDate = new Date('December 21, 2023 00:00:00 GMT+00:00');
       const currentDate = new Date();
-
+    
       const difference = targetDate - currentDate;
-
+    
       const daysRemaining = Math.floor(difference / (1000 * 60 * 60 * 24));
-
+      const hoursRemaining = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutesRemaining = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      const secondsRemaining = Math.floor((difference % (1000 * 60)) / 1000);
+    
       const resultContainer = document.getElementById('countdown-result');
-      resultContainer.textContent = `D-Day: ${daysRemaining} days until December 21st.`;
+      resultContainer.textContent = `D-Day: ${daysRemaining} days, ${hoursRemaining} hours, ${minutesRemaining} minutes, and ${secondsRemaining} seconds until December 21st.`;
     }
-
+    
