f7396f1c910bbe4b38d38f48a78a4145a57962f9
kims455 <kims455@newschool.edu>
2023-11-27 19:55:11 -0500

    Entry 10 Revision #3

--- harmonic-collection/entries/entry10/main.js
+++ harmonic-collection/entries/entry10/main.js
@@ -1,1 +1,28 @@
-js
+    // JavaScript to display homesickness instructions and update the clock
+    function displayHomesicknessInstructions() {
+      const instructions = [
+        "Be ready to hug your family.",
+        "Fasten your seatbelt of memories."
+
+      ];
+
+      const randomIndex = Math.floor(Math.random() * instructions.length);
+      document.getElementById('instructions').textContent = instructions[randomIndex];
+    }
+
+    function updateTime() {
+      const now = new Date();
+      const hours = now.getHours().toString().padStart(2, '0');
+      const minutes = now.getMinutes().toString().padStart(2, '0');
+      const seconds = now.getSeconds().toString().padStart(2, '0');
+
+      document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+    }
+
+    setInterval(updateTime, 1000);
+    displayHomesicknessInstructions();
+
+
+
+
+
