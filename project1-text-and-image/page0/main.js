8a4b590f61ec65d90c643b7efbc1c76370453ba9
kims455 <kims455@newschool.edu>
2024-02-07 15:57:51 -0500

    Project 1 Text and Image Revision 8

--- project1-text-and-image/page0/main.js
+++ project1-text-and-image/page0/main.js
@@ -1,3 +1,34 @@
+// Mouse tracer
+const tracer = document.getElementById("tracer");
+const dots = [];
+
+document.addEventListener("mousemove", function(event) {
+  const dot = document.createElement("div");
+  dot.className = "dot";
+  dot.style.left = event.clientX + "px";
+  dot.style.top = event.clientY + "px";
+
+  tracer.appendChild(dot);
+  dots.push(dot);
+
+
+  if (dots.length > 50) {
+    tracer.removeChild(dots.shift());
+  }
+});
+
+
+function removeDots() {
+  if (dots.length > 0) {
+    tracer.removeChild(dots.shift());
+    setTimeout(removeDots, 90);
+  }
+}
+
+removeDots();
+
+
+/*
 let currentIndex = 0;
 const sentences = document.querySelectorAll('.overlay-text');
 
@@ -26,3 +57,5 @@ document.querySelector('.paper-dove1').addEventListener('click', function() {
 document.querySelector('.paper-dove2').addEventListener('click', function() {
     showNextSentence();
 });
+
+*/
