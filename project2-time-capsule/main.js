87d6c5e67ee35a457885acda7f1be6b02e09c0bd
kims455 <kims455@newschool.edu>
2024-03-06 16:47:02 -0500

    Project 2 Time Capsule Revision 9

---
+++ project2-time-capsule/main.js
@@ -0,0 +1,18 @@
+// var counter = 1;
+
+// $(window).click(function(event) {
+//   $(".first").addClass("red");
+//   $(".second").addClass("green");
+//   $("#third").addClass("blue");
+//   $("body").append("<p>Hi, I'm paragraph number " + counter + "!</p>");
+//   counter++;
+// });
+
+$(window).mousemove(function(event) {
+  console.log(event.pageX + "px");
+  console.log(event.pageY + "px");
+  $("h1").css("top",event.pageY + "px");
+  $("h1").css("left",event.pageX + "px");
+});
+
+
