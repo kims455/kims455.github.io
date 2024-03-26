21d1ef971849db44fc23a9d07c777d9e568b5cf4
kims455 <kims455@newschool.edu>
2024-03-26 19:55:18 -0400

    Project 2 Time Capsule Revision 19

--- project2-time-capsule/main.js
+++
@@ -1,6 +0,0 @@
-$(window).mousemove(function(event) {
-  console.log(event.pageX + "px");
-  console.log(event.pageY + "px");
-  $("h1").css("top",event.pageY + "px");
-  $("h1").css("left",event.pageX + "px");
-});
